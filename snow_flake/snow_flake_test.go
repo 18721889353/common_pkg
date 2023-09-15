e676be120d509c652024c4b2aa20119f1479c698
sun <418406417@qq.com>
2023-09-15 23:11:44 +0800

    test

--- snow_flake/snow_flake_test.go
+++ snow_flake/snow_flake_test.go
@@ -1,29 +1,12 @@
 package snow_flake
 
 import (
-	"common_pkg/snow_flake/snowflake_interf"
-	"time"
-
-	sf "github.com/bwmarrin/snowflake"
+	"testing"
 )
 
-type snowflake struct {
-	node *sf.Node
-}
-
-func NewSnowflake(machineID int64) (snowflake_interf.InterfaceSnowFlake, error) {
-	var st time.Time
-	st, err := time.Parse("2006-01-02", "2023-01-01")
-	if err != nil {
-		return nil, err
-	}
-	sf.Epoch = st.UnixNano() / 1000000
-	node, err := sf.NewNode(machineID)
-	return &snowflake{
-		node: node,
-	}, nil
-}
-
-func (s *snowflake) GetId() int64 {
-	return s.node.Generate().Int64()
+func TestGetId(t *testing.T) {
+	newSnowflake, err := NewSnowflake(1)
+	t.Log(err)
+	id := newSnowflake.GetId()
+	t.Log(id)
 }
